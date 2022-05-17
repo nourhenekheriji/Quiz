992d0fa6d408cc9761b02241981976b74286c09a
nourhene kheriji <91379867+nourhenekheriji@users.noreply.github.com>
2022-05-17 15:26:50 +0200

    master commit

---
+++ js/question.js
@@ -0,0 +1,141 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+   
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+
+ {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Java est un langage développé par :",
+    answer: "Sun Microsystems",
+    options: [
+      "Apple",
+      "Sun Microsystems",
+      "Microsoft",
+      "Oracle"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Compiler du Java donne :",
+    answer: "Du byte code",
+    options: [
+      "Rien",
+      "Un fichier.jve",
+      "Du byte code",
+      "Un exécutable"
+    ]
+  },
+
+
+  {
+    numb: 8,
+    question: "Quel environnement de développement est propsé par Java?",
+    answer: "JDK",
+    options: [
+      "JRE",
+      "JVM",
+      "JSD",
+      "JDK"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Une variable de classe, commune à toutes les instances d'une classe doit être décarée :",
+    answer: "Static",
+    options: [
+      "Public",
+      "Private",
+      "Global",
+      "Static"
+    ]
+  },
+
+
+
+  {
+    numb: 10,
+    question: "Que signifie JRE?",
+    answer: "Java Runtime Environment",
+    options: [
+      "Java Runtime Engine",
+      "Java Realtime Execution",
+      "Java Runtime Environment",
+      "Java Routine Emulator"
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
